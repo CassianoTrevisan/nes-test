01e49d3a20be273946515ac12aada8b83aed9f6a
Cassiano_Ricardo_Tre <cassianotrevisan@yahoo.com.br>
2020-03-11 12:48:31 +0100

    first commit

---
+++ src/main/java/com/techprimers/springbatchexample1/services/StartJobService.java
@@ -0,0 +1,40 @@
+package com.techprimers.springbatchexample1.services;
+
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class StartJobService {
+
+    @Autowired
+    JobLauncher jobLauncher;
+
+    @Autowired
+    Job job;
+
+    public BatchStatus load() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+
+
+        Map<String, JobParameter> maps = new HashMap<>();
+        maps.put("time", new JobParameter(System.currentTimeMillis()));
+        JobParameters parameters = new JobParameters(maps);
+        JobExecution jobExecution = jobLauncher.run(job, parameters);
+
+        System.out.println("JobExecution: " + jobExecution.getStatus());
+
+        System.out.println("Batch is Running...");
+        while (jobExecution.isRunning()) {
+            System.out.println("...");
+        }
+
+        return jobExecution.getStatus();
+    }
+}
---
+++ src/main/java/com/techprimers/springbatchexample1/startup/CommandLineAppStartupRunner.java
@@ -0,0 +1,28 @@
+package com.techprimers.springbatchexample1.startup;
+
+import com.techprimers.springbatchexample1.services.StartJobService;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLineAppStartupRunner implements CommandLineRunner {
+    @Autowired
+    private StartJobService startJobService;
+
+    @Override
+    public void run(String... args) throws JobParametersInvalidException,
+            JobExecutionAlreadyRunningException,
+            JobRestartException,
+            JobInstanceAlreadyCompleteException {
+
+        for (int i = 0; i < args.length; ++i) {
+            System.out.println("args --->>>" + args[i]);
+        }
+        startJobService.load();
+    }
+}
